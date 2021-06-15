65b2885594892fe7d947ad21616c2d8b493f5f3a
arun1592 <arunpratap1592@gmail.com>
2021-06-15 20:34:27 +0000

    my first list

--- src/main/java/com/example/healthcare/HealthcareApplication.java
+++ src/main/java/com/example/healthcare/HealthcareApplication.java
@@ -2,10 +2,18 @@ package com.example.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HealthcareApplication {
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareApplication.class, args);
 	}
